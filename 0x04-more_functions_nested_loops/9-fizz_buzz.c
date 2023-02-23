3ad753619176824226edbf6d02d56b1d5654638c
Cosbyk <mwathikuria@gmail.com>
2023-02-23 15:01:50 -0800

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,6 +13,7 @@ int main(void)
 int x;
 
 for (x = 1; x <= 100; x++)
+
 {
 if ((x % 3 == 0) && (x % 5 == 0))
 {
@@ -28,7 +29,7 @@ printf("Buzz");
 }
 else
 {
-printf("%d" , x);
+printf("%d", x);
 }
 if (x != 100)
 {
@@ -37,4 +38,5 @@ printf(" ");
 }
 printf("\n");
 return (0);
+
 }
