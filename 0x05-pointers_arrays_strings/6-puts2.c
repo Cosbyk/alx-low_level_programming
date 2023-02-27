0dc9cb9ea157c2ad6bb620a63585814ea8592ff8
Cosbyk <mwathikuria@gmail.com>
2023-02-27 11:07:49 -0800

    6. Half the lies they tell about me aren't true

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,34 +1,26 @@
 #include "main.h"
-/**
- * puts_half - prints half of a string
- * @str: the string to print
- * Return: void
- */
 
-void puts_half(char *str)
+/**
+* puts2 - Prints every other character of a string
+* @str: the string to be treated
+* Return: void
+*/
+
+void puts2(char *str)
 
 {
 
+int i;
 int j = 0;
-int k;
 
 while (str[j] != '\0')
 {
 j++;
 }
-if (j % 2 == 1)
-{
-k = (j - 1) / 2;
-k += 1;
-}
-else
-{
-k = j / 2;
-}
 
-for (; k < j; k++)
+for (i = 0; i < j; i += 2)
 {
-_putchar(str[k]);
+_putchar(str[i]);
 }
 _putchar('\n');
 }
